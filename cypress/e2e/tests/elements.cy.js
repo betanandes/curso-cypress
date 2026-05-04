d7c8e3e935c767325ebcf76a6e6228d8865410fa
betanandes <robertafernandes46@gmail.com>
2026-05-04 14:40:00 -0300

    links

--- cypress/e2e/tests/elements.cy.js
+++ cypress/e2e/tests/elements.cy.js
@@ -8,4 +8,14 @@ describe('Work with nasic elements', () => {
         cy.get('.facilAchar').should('contain', 'Cuidado')
         cy.get('.facilAchar').should('have.text', 'Cuidado onde clica, muitas armadilhas...') //have.text é mais especifico, tem que ser exatamente igual
     })
+
+    it.only('Links', () => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+        // cy.get('a').click() (não vai funcionar porque tem mais de um link, tem que ser mais específico)
+
+        cy.reload() //recarrega a página
+        cy.get('#resultado').should('have.not.text', 'Voltou!')
+        cy.contains('Voltar').click()
+        cy.get('#resultado').should('have.text', 'Voltou!') //tem que ser exatamente igual
+    })
 })
