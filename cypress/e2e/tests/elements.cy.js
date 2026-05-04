8ba58bec287d389ce53a05e7943c767145999ad0
betanandes <robertafernandes46@gmail.com>
2026-05-04 13:45:25 -0300

    aula sobre elements

---
+++ cypress/e2e/tests/elements.cy.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+describe('Work with nasic elements', () => {
+    it ('Text', () => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+        cy.get('body').should('contain', 'Cuidado')
+        cy.get('span').should('contain', 'Cuidado')
+        cy.get('.facilAchar').should('contain', 'Cuidado')
+        cy.get('.facilAchar').should('have.text', 'Cuidado onde clica, muitas armadilhas...') //have.text é mais especifico, tem que ser exatamente igual
+    })
+})
