a2e66d8011e78144c79d9b46ce0f16ff750c48ff
betanandes <robertafernandes46@gmail.com>
2026-05-05 17:23:09 -0300

    tests hooks

--- cypress/e2e/tests/elements.cy.js
+++ cypress/e2e/tests/elements.cy.js
@@ -1,20 +1,25 @@
 /// <reference types="cypress" />
 
-describe('Work with nasic elements', () => {
-    it ('Text', () => {
+describe('Work with basic elements', () => {
+    beforeEach(() => {
         cy.visit('https://wcaquino.me/cypress/componentes.html')
+    }) // beforeEach é para rodar a página antes de cada teste
+    
+    beforeEach(() => {
+        cy.reload() // recarrega a página antes de cada teste
+    })
+
+    it('text', () => {
         cy.get('body').should('contain', 'Cuidado')
         cy.get('span').should('contain', 'Cuidado')
         cy.get('.facilAchar').should('contain', 'Cuidado')
-        cy.get('.facilAchar').should('have.text', 'Cuidado onde clica, muitas armadilhas...') //have.text é mais especifico, tem que ser exatamente igual
+        cy.get('.facilAchar').should('have.text', 'Cuidado onde clica, muitas armadilhas...') //have.text é mais especifico, tem que ser exatamente igual'
     })
 
-    it.only('Links', () => {
-        cy.visit('https://wcaquino.me/cypress/componentes.html')
+    it('links', () => {
         // cy.get('a').click() (não vai funcionar porque tem mais de um link, tem que ser mais específico)
 
-        cy.reload() //recarrega a página
-        cy.get('#resultado').should('have.not.text', 'Voltou!')
+        cy.get('#resultado').should('have.not.text' , 'Voltou!') //verifica se o elemento não existe
         cy.contains('Voltar').click()
         cy.get('#resultado').should('have.text', 'Voltou!') //tem que ser exatamente igual
     })
