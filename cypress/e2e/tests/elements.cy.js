b815e79c14897d66b3a23718b616b11bda91fb76
betanandes <robertafernandes46@gmail.com>
2026-05-05 18:49:23 -0300

    testes com textos

--- cypress/e2e/tests/elements.cy.js
+++ cypress/e2e/tests/elements.cy.js
@@ -9,18 +9,41 @@ describe('Work with basic elements', () => {
         cy.reload() // recarrega a página antes de cada teste
     })
 
-    it('text', () => {
+    it('Text', () => {
         cy.get('body').should('contain', 'Cuidado')
         cy.get('span').should('contain', 'Cuidado')
         cy.get('.facilAchar').should('contain', 'Cuidado')
         cy.get('.facilAchar').should('have.text', 'Cuidado onde clica, muitas armadilhas...') //have.text é mais especifico, tem que ser exatamente igual'
     })
 
-    it('links', () => {
+    it('Links', () => {
         // cy.get('a').click() (não vai funcionar porque tem mais de um link, tem que ser mais específico)
 
         cy.get('#resultado').should('have.not.text' , 'Voltou!') //verifica se o elemento não existe
         cy.contains('Voltar').click()
         cy.get('#resultado').should('have.text', 'Voltou!') //tem que ser exatamente igual
+    })
+
+    it.only('TextFields', () => {
+        cy.get('#formNome').type('Cypress Test')
+        cy.get('#formNome').should('have.value', 'Cypress Test') //verifica se o valor do campo é igual ao que foi digitado
+
+        cy.get('#elementosForm\\:sugestoes')
+            .type('textarea')
+            .should('have.value', 'textarea')
+
+
+        cy.get('#tabelaUsuarios > :nth-child(2) > :nth-child(1) > :nth-child(6) > input')
+            .type('???')
+
+
+        cy.get('[data-cy="dataSobrenome"]')
+            .type('Teste12345{backspace}{backspace}') //apaga os dois últimos caracteres
+            .should('have.value', 'Teste123')
+
+         cy.get('#elementosForm\\:sugestoes')
+            .clear()
+            .type('Erro{selectall}acerto', {delay: 100}) //seleciona todo o texto e digita por cima
+            .should('have.value', 'acerto')
     })
 })
