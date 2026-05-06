f811a5a2f777177fe4763dfa8b4a4a382bbd316b
betanandes <robertafernandes46@gmail.com>
2026-05-06 16:44:22 -0300

    radio buttons e busca por propriedade

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    viewportWidth: 1000,
+    viewportHeight: 660,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/tests/elements.cy.js
+++ cypress/e2e/tests/elements.cy.js
@@ -24,7 +24,7 @@ describe('Work with basic elements', () => {
         cy.get('#resultado').should('have.text', 'Voltou!') //tem que ser exatamente igual
     })
 
-    it.only('TextFields', () => {
+    it('TextFields', () => {
         cy.get('#formNome').type('Cypress Test')
         cy.get('#formNome').should('have.value', 'Cypress Test') //verifica se o valor do campo é igual ao que foi digitado
 
@@ -45,5 +45,15 @@ describe('Work with basic elements', () => {
             .clear()
             .type('Erro{selectall}acerto', {delay: 100}) //seleciona todo o texto e digita por cima
             .should('have.value', 'acerto')
+    })
+
+    it('RadioButton', () => {
+        cy.get('#formSexoFem')
+        .click()
+        .should('be.checked') //verifica se o radio button está selecionado
+        
+        cy.get('#formSexoMasc').should('not.be.checked') //verifica se o radio button não está selecionado
+        cy.get('[name="formSexo"]').should('have.length', 2) //verifica se existem dois radio buttons com o mesmo name
+
     })
 })
